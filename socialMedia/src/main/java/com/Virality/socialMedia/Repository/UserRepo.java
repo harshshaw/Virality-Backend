44323d001d7db9be4d4e5fd1fa6d3b824ef95603
Harsh shaw <harshshaw5@gmail.com>
2023-07-03 01:21:51 +0530

    added random in numberOfDaysInactive to test algorithm

--- socialMedia/src/main/java/com/Virality/socialMedia/Repository/UserRepo.java
+++ socialMedia/src/main/java/com/Virality/socialMedia/Repository/UserRepo.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends CrudRepository<Users,Long> {
+//    public UserDto algorithmParameters(){
+//
+//    }
 }
--- socialMedia/src/main/java/com/Virality/socialMedia/Services/algorithmService.java
+++ socialMedia/src/main/java/com/Virality/socialMedia/Services/algorithmService.java
@@ -8,13 +8,14 @@ public class algorithmService {
     private Integer numberOfLikes=100;
     private Integer numberOfPost=100;
     private Integer numberOfFollowers=100;
-    private Integer numberDaysInactive=3;
+    private Double numberDaysInactive=Math.random();
+
 
 
     //fetch values from DB
     public Double calculation(){
         baseValue=baseValue+(baseValue*((numberOfLikes+numberOfFollowers+numberOfPost)/numberDaysInactive*1.2));
-
+        System.out.println(numberDaysInactive);
         //We want to keep baseValue at 0.10 even after long period of inactiveness
         //since this is the minimun it can go for a User
         if(baseValue<0.10)
