2b6ceadd4141a1172be2ba9b88634d6504d62de6
sai-venkat17 <ramadugula.venkat@gmail.com>
2023-07-14 16:18:37 +0530

    changed algorithmService according to my changes

--- socialMedia/src/main/java/com/Virality/socialMedia/Controller/Algorithm.java
+++ socialMedia/src/main/java/com/Virality/socialMedia/Controller/Algorithm.java
@@ -1,20 +1,23 @@
-//package com.Virality.socialMedia.Controller;
-//
-//import com.Virality.socialMedia.Services.algorithmService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/v1")
-//public class Algorithm {
-//    @Autowired
-//    private algorithmService algorithmService;
-//
-//    @GetMapping("/algorithm")
-//    public Double algo()
-//    {
-//        return algorithmService.calculation();
-//    }
-//}
+package com.Virality.socialMedia.Controller;
+
+import com.Virality.socialMedia.Services.algorithmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Algorithm {
+    @Autowired
+    private algorithmService algorithmService;
+
+    @GetMapping("/algorithm/{userId}")
+    public Double algo(@PathVariable("userId") UUID userId)
+    {
+        return algorithmService.calculation(userId);
+    }
+}
