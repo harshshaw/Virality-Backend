f27012a14b6b3504f4a676e876607c8597e6bacf
Harsh shaw <harshshaw5@gmail.com>
2023-08-28 00:41:10 +0530

    updating Post feature added

--- socialMedia/src/main/java/com/Virality/Application/Controller/PostController.java
+++ socialMedia/src/main/java/com/Virality/Application/Controller/PostController.java
@@ -1,6 +1,7 @@
 package com.Virality.Application.Controller;
 
 import com.Virality.Application.Dto.PostDto;
+import com.Virality.Application.Entity.Post;
 import com.Virality.Application.Services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
--- socialMedia/src/main/java/com/Virality/Application/Services/PostService.java
+++ socialMedia/src/main/java/com/Virality/Application/Services/PostService.java
@@ -77,15 +77,19 @@ public class PostService {
         storageService.delete(postFilePath);
     }
 
+    //This service is updating numbers of likes on a post only
     public void updatePost(UUID postId,PostDto updatePost){
         Optional<Post> postFromDb=postRepo.findById(postId);
+        Integer likesUpdate=updatePost.getLikesCount();
+        System.out.println(likesUpdate);
+        System.out.println(updatePost.getUserId());
         if(postFromDb.isPresent())
         {
             Post postObject=postFromDb.get();
-//            postObject.
+            postObject.setLikesCount(likesUpdate+postObject.getLikesCount());
+            postRepo.save(postObject);
+
         }
-        System.out.println((postFromDb.isPresent()));
-        System.out.println(postFromDb);
     }
 
 
